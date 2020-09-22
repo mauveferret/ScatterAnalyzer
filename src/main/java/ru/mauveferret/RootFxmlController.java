f575d975c01bfed2f9eff7305e7f77f9439908cc
mauveferret <mauveferret@gmail.com>
2020-09-22 19:18:51 +0300

    some cosmetics changes

--- src/main/java/ru/mauveferret/RootFxmlController.java
+++ src/main/java/ru/mauveferret/RootFxmlController.java
@@ -16,35 +16,55 @@ import java.util.ArrayList;
 public class RootFxmlController {
 
     //FileChooser chooser = new FileChooser();
-    private boolean accessGranted=true;
+    //private boolean accessGranted=true;
     ParticleInMatterCalculator yourCalcuator;
 
     //Buttons for calculating
     @FXML
     private CheckBox getTXT;
-    @FXML
-    private CheckBox NER;
-    @FXML
-    private CheckBox NEY;
-    @FXML
-    private CheckBox NThetaR;
-    @FXML
-    private CheckBox NThetaY;
-    @FXML
-    private CheckBox NThetaPhiR;
-    @FXML
-    private CheckBox NThetaPhiY;
-    @FXML
-    private TextField StringCount;
-    @FXML
-    private ChoiceBox CalcType;
 
+    //energy distributions
+    @FXML
+    private CheckBox NEB;
+    @FXML
+    private CheckBox NES;
+    @FXML
+    private CheckBox NEI;
+    @FXML
+    private CheckBox NET;
+
+    //polar distributions
+    @FXML
+    private CheckBox NThetaB;
+    @FXML
+    private CheckBox NThetaS;
+    @FXML
+    private CheckBox NThetaT;
+    @FXML
+    private CheckBox NThetaI;
+
+    //angle map
+    @FXML
+    private CheckBox NThetaPhiB;
+    @FXML
+    private CheckBox NThetaPhiS;
+    @FXML
+    private CheckBox NThetaPhiI;
+    @FXML
+    private CheckBox NThetaPhiT;
+
+    //other
+    @FXML
+    private CheckBox depthProfile;
 
 
     ///////////////////////////
 
     @FXML
     private TextField time;
+    @FXML
+    private TextField codeName;
+
     @FXML
     private Label secret;
     @FXML
@@ -92,10 +112,6 @@ public class RootFxmlController {
     private TextField numberOfParticlesInScatter;
     @FXML
     TextField energyScattering;
-    //TRIM
-    @FXML
-    private Slider FileType;
-
 
     String path="lol";
     File file;
@@ -103,13 +119,9 @@ public class RootFxmlController {
     @FXML
     public void readme()
     {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                new GUI().showHelpPage("readme.png");
-            }
-        });
+        Platform.runLater(() -> new GUI().showHelpPage("readme.png"));
     }
+
     @FXML
     public void pushed() {
 
@@ -147,32 +159,20 @@ public class RootFxmlController {
                     initialize = yourCalcuator.initializeVariables();
                     if (!initialize.equals("OK")) {
                         System.out.println(initialize);
-                        System.out.println("PIZDETS");
-                    } else System.out.println("SDTrimSP initialized");
-                } else System.out.println("TRIM initialized");
-            } else System.out.println("SCATTER initialized");
-
-
+                        new GUI().showNotification("ERROR: "+initialize);
+                    } else codeName.setText("SDTrimSP");
+                } else codeName.setText("TRIM");
+            } else codeName.setText("SCATTER");
 
             numberOfParticlesInScatter.setText(yourCalcuator.projectileAmount + "");
             E0.setText(yourCalcuator.projectileMaxEnergy + "");
+            energyResolution.setText((yourCalcuator.projectileMaxEnergy/100)+"");
             polarAngleNE.setText(yourCalcuator.projectileIncidentPolarAngle + "");
             azimuthAngleNE.setText(yourCalcuator.projectileIncidentAzimuthAngle + "");
-            switch (yourCalcuator.calculatorType) {
-                case "SCATTER":
-                    FileType.setValue(0);
-                    break;
-                case "SDTrimSP":
-                    FileType.setValue(50);
-                    break;
-                case "TRIM":
-                    FileType.setValue(100);
-                    break;
-            }
         }
         catch (Exception e)
         {
-            System.out.println(e.getMessage());
+            System.out.println("175: "+e.getMessage());
         }
 }
 
@@ -222,7 +222,7 @@ public class RootFxmlController {
         try {
             double t = Double.parseDouble(dPolarAngleNE.getText());
             if (t <= 0 ) {
-                dPolarAngleNE.setText("5");
+                dPolarAngleNE.setText("2");
 
                 new GUI().showNotification("Установите разброс по углу больше нуля!");
 
@@ -230,12 +230,12 @@ public class RootFxmlController {
         }
         catch (Exception e)
         {
-            dPolarAngleNE.setText("5");
+            dPolarAngleNE.setText("2");
         }
         try {
             double t = Double.parseDouble(dAzimuthAngleNE.getText());
             if (t <= 0 ) {
-                dAzimuthAngleNE.setText("5");
+                dAzimuthAngleNE.setText("2");
 
                 new GUI().showNotification("Установите разброс по углу больше нуля!");
 
@@ -243,12 +243,12 @@ public class RootFxmlController {
         }
         catch (Exception e)
         {
-            dAzimuthAngleNE.setText("5");
+            dAzimuthAngleNE.setText("2");
         }
         try {
             double t = Double.parseDouble(dAzimuthAngleNtheta.getText());
             if (t <= 0 ) {
-                dAzimuthAngleNtheta.setText("5");
+                dAzimuthAngleNtheta.setText("2");
 
                 new GUI().showNotification("Установите разброс по углу больше нуля!");
 
@@ -275,7 +275,7 @@ public class RootFxmlController {
         }
         catch (Exception e)
         {
-            E0.setText("9000");
+            E0.setText("25000");
         }
 
     }
@@ -283,12 +283,7 @@ public class RootFxmlController {
     @FXML
     public void YouDoWhatYouGonnaDo()
     {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                new GUI().Futurama();
-            }
-        });
+        Platform.runLater(() -> new GUI().Futurama());
     }
     @FXML
     public void ResChanged()
@@ -299,7 +294,7 @@ public class RootFxmlController {
         }
         catch (Exception e)
         {
-            energyResolution.setText("90");
+            energyResolution.setText("250");
         }
     }
 
@@ -309,18 +304,12 @@ public class RootFxmlController {
         if (secret.getText().equals("А не попить ли чаю?")) secret.setText("");
         else
         secret.setText("А не попить ли чаю?");
-
     }
 
     @FXML
     public  void showHelp()
     {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                new GUI().showHelpPage("axes.png");
-            }
-        });
+        Platform.runLater(() -> new GUI().showHelpPage("axes.png"));
 
     }
 
@@ -330,109 +319,96 @@ public class RootFxmlController {
 
         if (path.equals("lol"))
         {
-            Platform.runLater(new Runnable() {
-                @Override
-                public void run() {
-                    new GUI().showNotificationAboutFile();
-                }
-            });
+            Platform.runLater(() -> new GUI().showNotificationAboutFile());
         }
 
-        else  if (accessGranted)
-        {
+            new Thread(() -> {
+                double E;
+                if (!E0.getText().equals("Ждите"))
+                     E=Double.parseDouble(E0.getText());
+                else E=yourCalcuator.projectileMaxEnergy;
+
+                //int energyReturnValue = E;
+                double dE = Double.parseDouble(energyResolution.getText());
+                double thetaNE = Double.parseDouble(polarAngleNE.getText());
+                double dThetaNE = Double.parseDouble(dPolarAngleNE.getText());
+                double phiNE=Double.parseDouble(azimuthAngleNE.getText());
+                double dphiNE=Double.parseDouble(dAzimuthAngleNE.getText());
+
+                ///////////
+
+                double dThetaNTheta=Double.parseDouble(dPolarAngleNtheta.getText());
+                double phiNTheta=Double.parseDouble(azimuthAngleNtheta.getText());
+                double dPhiNTheta=Double.parseDouble(dAzimuthAngleNtheta.getText());
+                double NThetadPhi1 = Double.parseDouble(NThetadPhi.getText());
+                double NdThetaPhi1 = Double.parseDouble(NdThetaPhi.getText());
+
+                E0.setText("Ждите");
+
+                //basicDistributions
+
+                ArrayList<Distribution> distributions = new ArrayList<>();
+
+                String sort = (NEB.isSelected()) ? "B" : "";
+                sort += (NES.isSelected()) ? "S" : "";
+                //sort += (NEI.isSelected()) ? "I" : "";
+                sort += (NET.isSelected()) ? "T" : "";
+
+                if (!sort.equals(""))
+                    distributions.add(new Energy(E,dE,phiNE, dphiNE, thetaNE, dThetaNE,sort, yourCalcuator));
+
+                sort = (NThetaB.isSelected()) ? "B" : "";
+                sort += (NThetaS.isSelected()) ? "S" : "";
+                sort += (NThetaS.isSelected()) ? "I" : "";
+                sort += (NThetaS.isSelected()) ? "T" : "";
+
+                if (!sort.equals(""))
+                    distributions.add(new Polar(phiNTheta, dPhiNTheta, dThetaNTheta,sort, yourCalcuator));
+
+                sort = (NThetaPhiB.isSelected()) ? "B" : "";
+                sort += (NThetaPhiS.isSelected()) ? "S" : "";
+                sort += (NThetaPhiI.isSelected()) ? "I" : "";
+                sort += (NThetaPhiT.isSelected()) ? "T" : "";
 
 
-            new Thread(new Runnable() {
+                if (!sort.equals(""))
+                    distributions.add(new AngleMap(dPhiNTheta, dThetaNTheta,sort, yourCalcuator));
 
-                @Override
-                public void run() {
-                    double E;
-                    if (!E0.getText().equals("Ждите"))
-                         E=Double.parseDouble(E0.getText());
-                    else E=yourCalcuator.projectileMaxEnergy;
+                //others
 
-                    //int energyReturnValue = E;
-                    int dE = Integer.parseInt(energyResolution.getText());
-                    double thetaNE = Double.parseDouble(polarAngleNE.getText());
-                    double dThetaNE = Double.parseDouble(dPolarAngleNE.getText());
-                    double phiNE=Double.parseDouble(azimuthAngleNE.getText());
-                    double dphiNE=Double.parseDouble(dAzimuthAngleNE.getText());
+                if (getTXT.isSelected()) distributions.add(new getTXT(yourCalcuator, ""));
 
-                    ///////////
+                yourCalcuator.postProcessCalculatedFiles(distributions);
+                yourCalcuator.printAndVisualizeData(distributions);
 
-                    double dThetaNTheta=Double.parseDouble(dPolarAngleNtheta.getText());
-                    double phiNTheta=Double.parseDouble(azimuthAngleNtheta.getText());
-                    double dPhiNTheta=Double.parseDouble(dAzimuthAngleNtheta.getText());
-                    double NThetadPhi1 = Double.parseDouble(NThetadPhi.getText());
-                    double NdThetaPhi1 = Double.parseDouble(NdThetaPhi.getText());
+                //Calculation is ended
 
-                    E0.setText("Ждите");
+                E0.setText(E+"");
+                //double initialCount=0;
+                //if (Double.parseDouble(numberOfParticlesInScatter.getText())<15) initialCount=Math.pow(10,  (int) (Math.ceil(Math.log10(data[1] + 0.5))) );
+               // else initialCount=Double.parseDouble(numberOfParticlesInScatter.getText());
+               count.setText(
+                        new BigDecimal(yourCalcuator.particleCount).setScale(5, RoundingMode.UP).doubleValue()+"");
+               time.setText(yourCalcuator.time/1000+"");
+                /*if ((Double.parseDouble(numberOfParticlesInScatter.getText())<15)&(!IsScatter)) initialCount=data[1];
+                scattered.setText(new BigDecimal(data[2]/initialCount).setScale(5, RoundingMode.UP).doubleValue()+"");
+                sputtered.setText(new BigDecimal(data[3]/initialCount).setScale(5, RoundingMode.UP).doubleValue()+"");
+                projected.setText(new BigDecimal(data[4]/initialCount).setScale(5, RoundingMode.UP).doubleValue()+"");
+                E0.setText(""+energyReturnValue);
+                energyScattering.setText(new BigDecimal(data[5]/(initialCount*E)).setScale(5, RoundingMode.UP).doubleValue()+"");
 
-                    //boolean IsScatter = (FileType.getValue() <50);
-
-                   /*double[] data = new Analyzer_DISABLED().calculateSpectra(path, E, dE , thetaNE ,dThetaNE, phiNE,dphiNE, 1, dThetaNTheta,
-                           phiNTheta, dPhiNTheta, NER.isSelected(), NEY.isSelected(), NThetaR.isSelected(),
-                           NThetaY.isSelected(), Integer.parseInt(StringCount.getText()), NThetaPhiR.isSelected(),NThetaPhiY.isSelected(), NThetadPhi1, NdThetaPhi1, getTXT.isSelected(), IsScatter);
-                   path="lol";
-                    */
-
-                    ArrayList<Distribution> distributions = new ArrayList<>();
-
-                    String sort = (NER.isSelected()) ? "B" : "";
-                    sort += (NEY.isSelected()) ? "S" : "";
-
-                    if (NER.isSelected() || NEY.isSelected())
-                        distributions.add(new Energy(E,dE,phiNE, dphiNE, thetaNE, dThetaNE,sort, yourCalcuator));
-
-                    sort = (NThetaR.isSelected()) ? "B" : "";
-                    sort += (NThetaY.isSelected()) ? "S" : "";
-
-                    if (NThetaR.isSelected() || NThetaY.isSelected())
-                        distributions.add(new Polar(phiNTheta, dPhiNTheta, dThetaNTheta,sort, yourCalcuator));
-
-                    sort = (NThetaPhiR.isSelected()) ? "B" : "";
-                    sort += (NThetaPhiY.isSelected()) ? "S" : "";
-
-                    if (NThetaPhiR.isSelected() || NThetaPhiY.isSelected())
-                        distributions.add(new AngleMap(dPhiNTheta, dThetaNTheta,sort, yourCalcuator));
-                    //TODO getTXT
-                    if (getTXT.isSelected()) distributions.add(new getTXT(yourCalcuator, ""));
-
-                    yourCalcuator.postProcessCalculatedFiles(distributions);
-                    yourCalcuator.printAndVisualizeData(distributions);
+                 */
 
 
-                    E0.setText(E+"");
-                    //double initialCount=0;
-                    //if (Double.parseDouble(numberOfParticlesInScatter.getText())<15) initialCount=Math.pow(10,  (int) (Math.ceil(Math.log10(data[1] + 0.5))) );
-                   // else initialCount=Double.parseDouble(numberOfParticlesInScatter.getText());
-                    time.setText(
-                            new BigDecimal(yourCalcuator.particleCount).setScale(5, RoundingMode.UP).doubleValue()+"");
-                    count.setText(yourCalcuator.particleCount+"");
-                    StringCount.setText(
-                            new BigDecimal(yourCalcuator.particleCount).setScale(5, RoundingMode.UP).doubleValue()+"");
+                scattered.setText((((double) yourCalcuator.scattered)/((double) yourCalcuator.particleCount))+"");
+                sputtered.setText((((double) yourCalcuator.sputtered)/((double) yourCalcuator.particleCount))+"");
+                implanted.setText((((double) yourCalcuator.implanted)/((double) yourCalcuator.particleCount))+"");
 
-                    /*if ((Double.parseDouble(numberOfParticlesInScatter.getText())<15)&(!IsScatter)) initialCount=data[1];
-                    scattered.setText(new BigDecimal(data[2]/initialCount).setScale(5, RoundingMode.UP).doubleValue()+"");
-                    sputtered.setText(new BigDecimal(data[3]/initialCount).setScale(5, RoundingMode.UP).doubleValue()+"");
-                    projected.setText(new BigDecimal(data[4]/initialCount).setScale(5, RoundingMode.UP).doubleValue()+"");
-                    E0.setText(""+energyReturnValue);
-                    energyScattering.setText(new BigDecimal(data[5]/(initialCount*E)).setScale(5, RoundingMode.UP).doubleValue()+"");
+                yourCalcuator.scattered = 0;
+                yourCalcuator.sputtered = 0;
+                yourCalcuator.implanted = 0;
+                yourCalcuator.particleCount = 0;
 
-                     */
-
-
-                    scattered.setText((((double) yourCalcuator.scattered)/((double) yourCalcuator.particleCount))+"");
-                    sputtered.setText((((double) yourCalcuator.sputtered)/((double) yourCalcuator.particleCount))+"");
-                    implanted.setText((((double) yourCalcuator.implanted)/((double) yourCalcuator.particleCount))+"");
-
-                    yourCalcuator.scattered = 0;
-                    yourCalcuator.sputtered = 0;
-                    yourCalcuator.implanted = 0;
-                    yourCalcuator.particleCount = 0;
-
-                }
             }).start();
-        }
     }
 }
